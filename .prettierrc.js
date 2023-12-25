82be24f48e546bbf9ec61919261fe5415d735904
03-Hexik <jurcenkojirka4@gmail.com>
2023-12-25 18:38:12 -0500

    Changed favicon, added navigation structure and style.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    useTabs: true,
+    tabWidth: 8,
+    semi: true,
+    singleQuote: true,
+    arrowParens: "avoid",
+};
+
+export default config;
