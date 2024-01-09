dcffccc8f06f2c6c5250877aac91c702c6f7a6ff
03-Hexik <jurcenkojirka4@gmail.com>
2024-01-10 00:41:16 +0100

    feat: Add Vue3Lottie plugin and configure main.js (main)
    - Import Vue3Lottie plugin in main.js file
    - Add plugin to the app instance
    - Mount the app to #VueApp element

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#VueApp');
+import Vue3Lottie from "vue3-lottie";
+/*import "vue3-lottie/dist/style.css";*/
+
+const app = createApp(App);
+
+app.use(router);
+app.use(Vue3Lottie);
+
+app.mount("#VueApp");
