3ad87209f045da47df99db206c8c6b5d423c6744
03-Hexik <jurcenkojirka4@gmail.com>
2024-01-09 12:30:40 +0100

    🔨refactor(main): remove imports for gsap modules
    The commit removes import statements for gsap modules in the main.js file.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from 'vue';
-import gsap from '@h3xik/gsap-mod';
-import { ScrollTrigger } from '@h3xik/gsap-mod/ScrollTrigger';
-import { ScrollToPlugin } from '@h3xik/gsap-mod/ScrollToPlugin';
-import { ScrollSmoother } from "@h3xik/gsap-mod/ScrollSmoother";
-
 import App from './App.vue';
 import router from './router';
-
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, ScrollSmoother);
 
 createApp(App).use(router).mount('#VueApp');
