88588672451b9575169b5345f5d5cf52381376f0
03-Hexik <jurcenkojirka4@gmail.com>
2024-01-10 15:06:48 +0100

    chore(main.js): remove unused import of Vue3Lottie to clean up code chore(main.js): remove commented out import of "vue3-lottie/dist/style.css" to improve code readability

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import Vue3Lottie from "vue3-lottie";
-/*import "vue3-lottie/dist/style.css";*/
-
 const app = createApp(App);
-
 app.use(router);
-app.use(Vue3Lottie);
-
 app.mount("#VueApp");
