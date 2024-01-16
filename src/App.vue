c47b6c2b5362c8a6a9737dd0fb06b1b136060c06
03-Hexik <jurcenkojirka4@gmail.com>
2024-01-16 17:47:08 +0100

    chore(index.html): remove unused Typekit stylesheet to improve page load performance feat(App.vue): refactor code to improve readability and maintainability feat(App.vue): update variable declarations to improve code organization feat(App.vue): update main section structure and add content feat(App.vue): update footer section structure and add content chore(_variables.scss): update color values for white and gray shades chore(_variables.scss): add commented out color value for black chore(_base.scss): remove unnecessary padding from body chore(_footer.scss): adjust margin and font size for footer title chore(_footer.scss): adjust transform property for lottie animation chore(_header.scss): adjust margin and height for header section chore(_navigation.scss): adjust padding and box-shadow for navigation component chore(main.scss): remove unused import statement chore(_mainpage.scss): adjust styles for main section and add skillset component

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,8 @@ const toggleScroll = () => {
 };
 
 const main = ref();
-let smoother;
-let ctx;
 let disableMain = ref(false);
+let smoother, ctx;
 
 onMounted(() => {
   ctx = gsap.context(() => {
@@ -37,19 +36,54 @@ onUnmounted(() => {
 
 <template ref="main">
   <NavigationComponent @toggleScroll="toggleScroll"/>
-  <div v-bind:class="{ 'disable': disableMain }" id="smooth-content">
-    
-    <section ref="main">
+
+  <div id="smooth-wrapper" v-bind:class="{ 'disable': disableMain }">
+    <div id="smooth-content">
       <header class="header" data-speed="1.5">
         <h1 class="header__title"><span>this.</span>jiří</h1>
         <h2 class="header__subtitle">Jiří Jurčenko</h2>
         <p class="header__caption">Student, Web Developer, Software Engineer.</p>
       </header>
-      
 
+      <main>
+        <article>
+          <h3>O mně</h3>
+          <p>
+            Je mi 17, žiju v České republice a rád tvořím software. Svůj zájem o programování jsem objevil už na
+            základní škole, kde jsem si oblíbil využívání své kreativity k vytváření webových stránek. Teď studuji druhý
+            ročník SPSŠE v Liberci a i když je to při studiu občas výzva, na programování si čas najdu vždycky. To, co
+            mě drží na této cestě, je, že vždy najdu nadšení v učení nových věcí, technologií a v řešení neznámých
+            výzev.
+            Kromě programování se zajímám i o správu sítí, serverů a celkově hardware.</p>
+        </article>
 
-    </section>
+        <article>
+          <h3>Skillset</h3>
 
-    <FooterComponent/>
+          <div class="skillset">
+            <span>Vue 3</span>
+            <span>JavaScript</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>Sass</span>
+            <span>Vite</span>
+            <span>Git</span>
+            <span>Jira</span>
+            <span>GitHub Pages</span>
+            <span>GSAP</span>
+            <span>GitHub</span>
+            <span>C# (.NET)</span>
+            <span>Razor Pages</span>
+            <span>EF Core</span>
+            <span>Python</span>
+            <span>Netlify</span>
+          </div>
+        </article>
+      </main>
+
+      <footer>
+        <FooterComponent/>
+      </footer>
+    </div>
   </div>
 </template>
