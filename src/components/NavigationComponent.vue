38cde02446a9943f03165987d51dfb1bfd5ec8cc
03-Hexik <jurcenkojirka4@gmail.com>
2024-01-18 00:26:08 +0100

    chore(NavigationComponent.vue): update logo link in navigation component to point to the current page instead of index.html for better user experience chore(NavigationComponent.vue): update GitHub link in navigation component to point to the correct GitHub profile chore(NavigationComponent.vue): update email link in navigation component to use a mailto link for easier contact chore(_navigation.scss): remove unnecessary CSS properties and adjust styling for better alignment and readability

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -15,15 +15,15 @@ onUnmounted(() => {
   <nav class="navigation" ref="main">
     <ul class="navigation__list">
       <li class="navigation__list__item">
-        <a href="index.html" class="logo">.Jiří()</a>
+        <a href="#">.Jiří()</a>
       </li>
-      <li class="navigation__list__item navigation__list__item--2">
+      <li class="navigation__list__item ml-auto">
         <a href="https://github.com/h3xik">
           <ion-icon name="logo-github"></ion-icon>
         </a>
       </li>
       <li class="navigation__list__item">
-        <a href="">
+        <a href="mailto:contact.jiri@pm.me">
           <ion-icon name="mail-outline"></ion-icon>
         </a>
       </li>
