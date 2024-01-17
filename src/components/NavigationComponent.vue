4a8971eda00587fe2b12057357e846ac5aadaee5
03-Hexik <jurcenkojirka4@gmail.com>
2024-01-17 23:38:24 +0100

    feat(NavigationComponent): Simplified the navigation menu
    Removed useless hamburger menu and moved the items to nav.

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -4,57 +4,7 @@ import {onMounted, onUnmounted, ref} from 'vue';
 import gsap from '@h3xik/gsap-mod';
 
 const main = ref();
-let tl;
-let ctx;
-
-const showMenu = function () {
-  tl.reversed(!tl.reversed());
-};
-
-onMounted(() => {
-  ctx = gsap.context(self => {
-    const links = self.selector('.navigation-mobile__item');
-    tl = gsap
-        .timeline()
-        .fromTo(
-            links[3],
-            {x: 130},
-            {x: 0, ease: 'Power4.inOut', duration: 0.3},
-            '>0.2'
-        )
-        .fromTo(
-            links[2],
-            {x: 130},
-            {
-              x: 0,
-              ease: 'Power4.inOut',
-              duration: 0.3,
-            },
-            '>0.2'
-        )
-        .fromTo(
-            links[1],
-            {x: 130},
-            {
-              x: 0,
-              ease: 'Power4.inOut',
-              duration: 0.3,
-            },
-            '>0.2'
-        )
-        .fromTo(
-            links[0],
-            {x: 130},
-            {
-              x: 0,
-              ease: 'Power4.inOut',
-              duration: 0.3,
-            },
-            '>0.2'
-        )
-        .reverse();
-  }, main.value);
-});
+let tl, ctx;
 
 onUnmounted(() => {
   ctx.revert();
@@ -67,59 +17,15 @@ onUnmounted(() => {
       <li class="navigation__list__item">
         <a href="index.html" class="logo">.Jiří()</a>
       </li>
+      <li class="navigation__list__item navigation__list__item--2">
+        <a href="https://github.com/h3xik">
+          <ion-icon name="logo-github"></ion-icon>
+        </a>
+      </li>
       <li class="navigation__list__item">
-        <button
-            aria-label="Navigation menu"
-            id="navigationMenuButton"
-            @click="$emit('toggleScroll'); showMenu();">
-          <ion-icon name="menu" aria-hidden="true"></ion-icon>
-        </button>
-
-        <ul class="navigation-mobile" id="navigationList">
-          <li class="navigation-mobile__item"
-              @click="$emit('toggleScroll'); showMenu();">
-            <a
-                href="mailto: jx.jiri@pm.me"
-                @click="
-								$emit('toggleScroll');
-								showMenu();
-							"
-            >.Jiří
-            </a>
-          </li>
-          <li class="navigation-mobile__item"
-              @click="$emit('toggleScroll'); showMenu();">
-            <a
-                href="mailto: jx.jiri@pm.me"
-                @click="
-								$emit('toggleScroll');
-								showMenu();
-							"
-            >.about
-            </a>
-          </li>
-          <li class="navigation-mobile__item">
-            <a
-                href="mailto: jx.jiri@pm.me"
-                @click="
-								$emit('toggleScroll');
-								showMenu();
-							"
-            >.email
-            </a>
-          </li>
-          <li class="navigation-mobile__item">
-            <a href="https://github.com/h3xik">
-              <ion-icon
-                  name="logo-github"
-                  @click="
-									$emit('toggleScroll');
-									showMenu();
-								"
-              ></ion-icon>
-            </a>
-          </li>
-        </ul>
+        <a href="">
+          <ion-icon name="mail-outline"></ion-icon>
+        </a>
       </li>
     </ul>
   </nav>
