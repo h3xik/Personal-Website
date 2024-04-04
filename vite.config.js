470740638daa194a6448e03fee40a65258784b10
h3xik <account.hexik@pm.me>
2024-04-04 11:49:48 +0200

    Uninstall unfonts.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import {defineConfig} from 'vite';
-import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-        plugins: [
-            Unfonts({
-                custom: {
-                    families: {
-                        'Noto Sans Mono': {
-                            src: './src/assets/fonts/NotoSansMono*',
-                        },
-                    },
-                    preload: true,
-                    display: 'swap'
-                },
-            })
-        ],
     };
 });
